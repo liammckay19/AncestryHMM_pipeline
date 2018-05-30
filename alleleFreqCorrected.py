21cfd5169aecf2429443cac91bf7667d40d329d3
Liam McKay <ltmckay@ucsc.edu>
2018-05-29 20:40:21 -0700

    got the allele freq calculator to print 119233

--- alleleFreqCorrected.py
+++ alleleFreqCorrected.py
@@ -37,29 +37,72 @@ def main():
     numAboveC = 0
     read = VCFreader("NewChr1.snp.vcf")
     c = sys.argv[1]
-    populationName = str(sys.argv[2])
+    populationName1 = str(sys.argv[2])
+    populationName2 = str(sys.argv[3])
     for line in read.readVCF():
-        denom = 0
-        numer = 0
+        denom1 = 0
+        numer1 = 0
+        denom2 = 0
+        numer2 = 0
         lineList = list(line.split('\t'))
         if lineList[0].startswith('##'):
             continue
         elif lineList[0].startswith('#'):
-            populationColumns = [ i for i, column in enumerate(lineList) if re.search('^'+populationName, column) ]
+            populationColumns1 = [ i for i, column in enumerate(lineList) if re.search('^'+populationName1, column) ]
+            populationColumns2 = [ i for i, column in enumerate(lineList) if re.search('^'+populationName2, column) ]
         else:
-            for column in populationColumns:
-                # print(lineList[column])
-                # print(int(lineList[column][:1]),lineList[column][2:3])
-                if (lineList[column][2:3] != '.'):
-                    denom += 1
-                if (lineList[column][:1] != '.'):
-                    numer += int(lineList[column][:1])
-            if denom == 0:
-                denom = 1 # so theres no dividing by zero
-            if denom > len(populationColumns)/2:
-                if numer/denom > float(c):
-                    numAboveC+=1
-                    # print("{}: {} {} {} {} {}/{}={}".format(populationName,lineList[0],lineList[1],lineList[3],lineList[4],numer,denom,numer/denom))
+            for column in range(0,max(len(populationColumns1),len(populationColumns2))):
+                colPop_1 = populationColumns1[column]
+                colPop_2 = populationColumns2[column]
+                # print(populationColumns1,populationColumns2)
+                # print((lineList[column][:1]),lineList[column][2:3])
+                if (lineList[colPop_1][2:3] != '.'): 
+                    if int(lineList[colPop_1][2:3])<=1:
+                        denom1 += 1
+
+                if (lineList[colPop_1][:1] != '.'): 
+                    if int(lineList[colPop_1][:1])<=1:
+                        numer1 += int(lineList[colPop_1][:1])
+                        denom1 += 1
+                if (lineList[colPop_2][2:3] != '.'):
+                    if int(lineList[colPop_2][2:3])<=1:
+                        denom2 += 1
+
+                if (lineList[colPop_2][:1] != '.'):
+                    if int(lineList[colPop_2][:1])<=1:
+                        numer2 += int(lineList[colPop_2][:1])
+                        denom2 += 1
+
+            if denom1 == 0:
+                denom1 = 1 # so theres no dividing by zero
+            if denom2 == 0:
+                denom2 = 1 # so theres no dividing by zero
+            chrom_number1 = denom1
+            chrom_number2 = denom2
+            # print(chrom_number2,chrom_number1)
+            if chrom_number1 < 14 or chrom_number2 < 14:
+                continue
+                # print("moving on1")
+            elif abs(numer1/denom1-numer2/denom2) < float(c):
+                continue
+            else :
+                # print("found allele: {}".format(lineList))
+                # print("{},{}: {} {} {} {}/{}={}|{}/{}={}, allelefreq={}".format(\
+                #             populationName1,populationName2,lineList[1],lineList[3],lineList[4],\
+                #             numer1,denom1,round(numer1/denom1,3),numer2,denom2,round(numer2/denom2,3),\
+                #             round(abs(numer1/denom1-numer2/denom2),3)))
+                # if numAboveC % 10000==0: print(numAboveC)
+                numAboveC += 1
+            # if numer1+numer2 >= 14:
+            #     if denom1 > len(populationColumns1)/2 and denom2 > len(populationColumns2)/2:
+            #         if abs(numer1/denom1-numer2/denom2) > float(c):
+                        # numAboveC+=1
+                        # print(lineList[colPop_1],lineList[colPop_2])
+
+                        # print("{},{}: {} {} {} {}/{}={}|{}/{}={}, allelefreq={}".format(\
+                        #     populationName1,populationName2,lineList[1],lineList[3],lineList[4],\
+                        #     numer1,denom1,round(numer1/denom1,3),numer2,denom2,round(numer2/denom2,3),\
+                        #     round(abs(numer1/denom1-numer2/denom2),3)))
     print(numAboveC)
 main()
 
