96b8b221c4db08e49edea2a57359c55d6bd94bde
Liam McKay <ltmckay@ucsc.edu>
2018-06-02 13:04:06 -0700

    userinterfaced and VCF reader

--- alleleFreqCorrected.py
+++ alleleFreqCorrected.py
@@ -21,30 +21,54 @@ class VCFreader :
         else:
             return open(self.fname)
     def readVCF (self):
-        ''' Read an entire FastA record and return the sequence header/sequence'''
-        header = ''
-        sequence = ''
+        ''' Read an entire VCF record and return the sequence header/sequence'''
+        line = ''
         with self.doOpen() as fileH:
-            # skip to first fasta header
             line = fileH.readline()
-            while not line.startswith('##') :
-                line = fileH.readline()
+            print(line)
             for line in fileH:
                 yield line
         yield line
 
 def main():
     numAboveC = 0
-    read = VCFreader("NewChr1.snp.vcf")
-    c = sys.argv[1]
-    populationName1 = str(sys.argv[2])
-    populationName2 = str(sys.argv[3])
+
+    
+    try:
+        c = float(sys.argv[1])
+        populationName1 = str(sys.argv[2])
+        populationName2 = str(sys.argv[3])
+        fileName = str(sys.argv[4])
+        read = VCFreader(fileName)
+        try:
+            f = open(fileName, 'r')
+        except OSError:
+            print('cannot open', fileName)
+            return
+        else:
+            print(fileName, 'has', len(f.readlines()), 'lines')
+            f.close()
+
+    except IndexError:
+        if(len(sys.argv) != 5):
+            print("Incorrect number of arguments. Usage:\n \
+    python alleleFreqCorrected.py [float c-value] [population name 1] [population name 2] [filename]\n")
+        else:
+            print("Usage: python alleleFreqCorrected.py [float c-value] [population name 1] [population name 2] [filename]")
+        return
+    except UnboundLocalError:
+        print("Could not read file: {}".format(fileName))
+        return
+        
+    
+
     for line in read.readVCF():
         denom1 = 0
         numer1 = 0
         denom2 = 0
         numer2 = 0
         lineList = list(line.split('\t'))
+        print(lineList)
         if lineList[0].startswith('##'):
             continue
         elif lineList[0].startswith('#'):
@@ -77,7 +101,7 @@ def main():
                 denom2 = 1 # so theres no dividing by zero
             chrom_number1 = denom1
             chrom_number2 = denom2
-            if chrom_number1 < 14 or chrom_number2 < 14:
+            if chrom_number1 < 14 or chrom_number2 < 14: # make parameter the code for min chromosome number
                 continue
             elif abs(numer1/denom1-numer2/denom2) < float(c):
                 continue
