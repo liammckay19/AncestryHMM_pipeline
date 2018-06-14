972c8a2b11b924fbb021c4cb2a1e2b4a9d1a2f79
Liam McKay <ltmckay@ucsc.edu>
2018-06-14 16:52:04 -0700

    multiple chromosome compatibility and multiple reference compatibility. Found test vcf data online and tested to iron out bugs

--- createAncestryHMM-Input.py
+++ createAncestryHMM-Input.py
@@ -32,30 +32,24 @@ class VCFreader :
                 while not line.startswith('##'):
                     yield line
                     line = fileH.readline()
-
+def pretty(d, indent=0): #https://stackoverflow.com/questions/3229419/how-to-pretty-print-nested-dictionaries
+   for key, value in d.items():
+      print('\t' * indent + str(key))
+      if isinstance(value, dict):
+         pretty(value, indent+1)
+      else:
+         print('\t' * (indent+1) + str(value))
+def file_len(fname): #https://stackoverflow.com/questions/845058/how-to-get-line-count-cheaply-in-python
+    with open(fname) as f:
+        for i, l in enumerate(f):
+            pass
+    return i + 1
 class FreqDistanceCalculator :
     def __init__(self):
         config = configparser.ConfigParser()
         config.read('config.ini')
-        print('Your config file input:\n',dict(config['DEFAULT'].items()))
-        usage="\nUsage: python createAncestryHMM-Input.py  \n"\
-                    +"Type python createAncestryHMM-Input.py -help to get descriptions of arguments\n "\
-                    +"You must have edited the config.ini file to read in parameters"
-        helpString = "[allelefreq cutoff]\n\t (Float) cutoff value for reference panel allele frequency calculation\n"\
-                    +"\n[min locus distance]\n\t (Integer) minimum distance between each allele locus\n" \
-                    +"\n[number of reference panels]\n\t (Integer) number of reference panel species in the VCF\n" \
-                    +"\n[recombination_rate]\n\t (Float) estimated recombination rate for recombination probability for Ancestry_HMM input.\n\t Average recombinations per base pairs\n" \
-                    +"\n[minChrom]\n\t (Integer) the minimum amount of chromosomes that must be present in the reference panel alleles to make it through the threshold\n" \
-                    +"\n[filename]\n\t (String no quotes) Name of VCF file on local machine\n" \
-                    +"\n[refPanel names(requires 2 names)]\n\t (at least 2 Strings no quotes) Names of reference panels in the VCF file.\n\t Reference panels should be named like guanaco0 guanaco1 guanaco2 etc.\n\t Example argument: guanaco\n" \
-                    +"\n[sample names]\n\t (at least 2 Strings no quotes) Names of sample panels to be run in Ancestry_HMM. \n\t Should be named like llama1 llama2 llama3 etc.\n\t Example argument: llama\n"
-        numAboveC = 0
-        
-        
-        if (len(sys.argv) <0):
-            if '-help' in sys.argv[1]:
-                print(helpString)
-                exit()
+        print('Your config file input:\n')
+        pretty(dict(config['DEFAULT']),0)
         self.c = float(config['DEFAULT']['allelefreq cutoff'])
         self.locusLength = int(config['DEFAULT']['min locus distance'])
         self.refPanelNumber = int(config['DEFAULT']['number of reference panels'])
@@ -63,7 +57,7 @@ class FreqDistanceCalculator :
         self.minChrom = int(config['DEFAULT']['minChrom'])
         self.fileName = str(config['DEFAULT']['filename'])
         self.populationNames = config['DEFAULT']['populationNames'].split(',')
-        print(self.populationNames)
+        self.lineCount = 0
 
     def createAncestryHMMInputFile(self, outFileName):
         '''
@@ -83,14 +77,17 @@ class FreqDistanceCalculator :
         numAboveC = 0
         outFile = open(outFileName, 'w')
         currentLocus = 0
+
         for line in self.read.readVCF(): # read line by line of VCF
-            reference0_count = 0;reference1_count = 0;reference0AlleleA = 0;
-            reference1AlleleA = 0;sampleAlleleA=0;sample_count=0
+            reference1_count = 0;reference2_count = 0;reference1AlleleA = 0;
+            reference2AlleleA = 0;sampleAlleleA=0;sample_count=0
             chromosomeNumber = 0
             position = 0
             recombinationFrequency = 0.0
 
             lineList = list(line.split('\t'))
+            pastChromosomeNumber = lineList[0].split('[A-Z][a-z]')[0]
+
             alleleCountRefAltList = []
             readCountRefAltList = []
             alleleFreqList = []
@@ -103,93 +100,120 @@ class FreqDistanceCalculator :
                 for populationName in self.populationNames:
                     populationColumnsList.append([ i for i, column in enumerate(lineList) if re.search('^'+populationName, column) ])
                 
+                if self.refPanelNumber > len(populationColumnsList):
+                    print('Count of references in config is larger than columns in vcf: ref='+str(self.refPanelNumber)+' col='+str(len(populationColumnsList))+'\nExiting')
+                    exit()
                 # check if all columns have been read
                 p = 0
                 initPopulationCount=len(populationColumnsList[0])
                 for populationColumn in populationColumnsList :
                     if populationColumn == [] :
-                        print("Could not find population "+self.populationNames[p]+'\nExiting')
-                        exit()
-                    elif len(populationColumn) != initPopulationCount:
-                        print("Different population sizes are not allowed "+self.populationNames[p]+'\nExiting')
-                        # exit()
+                        print("Could not find population "+self.populationNames[p]+'\nContinuing')
                     p += 1
                 # ref lists [0] [1] ... sample list [2] [3] ...
-                print(populationColumnsList)
             else: # actual data of VCF
                
                 for refpopulation in range(0,self.refPanelNumber-1): # compute allele count for at least two reference populations
-                    reference0_count=0
-                    reference0AlleleA=0
-                    print("refpopulation"+str(refpopulation)+'-'+str(refpopulation+1))
-                    for column in range(0,len(populationColumnsList[refpopulation])):
-                        refPop1 = populationColumnsList[refpopulation+1][column]
-                        print(refPop1)
-                        refPop2 = populationColumnsList[refpopulation][column]
-                        print(refPop2)
+                    reference1_count=0
+                    reference1AlleleA=0
+                    bothRefList = list(zip(populationColumnsList[refpopulation+1],populationColumnsList[refpopulation]))
+                    # print(populationColumnsList)
+                    # print((populationColumnsList[refpopulation+1],populationColumnsList[refpopulation]))
+                    # print(bothRefList)
 
-                        if (lineList[refPop1][:1] != '.'): 
-                            if int(lineList[refPop1][:1])<=1:
-                                reference0AlleleA += int(lineList[refPop1][:1])
-                                reference0_count += 1
-                        
-                        if (lineList[refPop1][2:3] != '.'): 
-                            if int(lineList[refPop1][2:3])<=1:
-                                reference0AlleleA += int(lineList[refPop1][2:3])
-                                reference0_count += 1
-
-                        if (lineList[refPop2][:1] != '.'):
-                            if int(lineList[refPop2][:1])<=1:
-                                reference1AlleleA += int(lineList[refPop2][:1])
+                    for column1,column2 in bothRefList:
+                        # get 1st chrom - 0/1
+                        if (lineList[column1][:1] != '.'): 
+                            if int(lineList[column1][:1])<=1:
+                                reference1AlleleA += int(lineList[column1][:1])
                                 reference1_count += 1
-                        
-                        if (lineList[refPop2][2:3] != '.'):
-                            if int(lineList[refPop2][2:3])<=1:
-                                reference1AlleleA += int(lineList[refPop2][2:3])
+                        # get 2nd chrom - 0/1
+                        if (lineList[column1][2:3] != '.'): 
+                            if int(lineList[column1][2:3])<=1:
+                                reference1AlleleA += int(lineList[column1][2:3])
                                 reference1_count += 1
-                                
-                    alleleCountRefAltList.append(abs(reference0_count-reference0AlleleA))
-                    alleleCountRefAltList.append(reference0AlleleA)
-                    alleleCountRefAltList.append(abs(reference1_count-reference1AlleleA))
-                    alleleCountRefAltList.append(reference1AlleleA)
 
-                if reference0_count == 0:
-                    reference0_count = 1 # so theres no dividing by zero
-                if reference1_count == 0:
-                    reference1_count = 1 # so theres no dividing by zero
-                chrom_number1 = reference0_count
-                chrom_number2 = reference1_count
-                if chrom_number1 < self.minChrom or chrom_number2 < self.minChrom: # make parameter the code for min chromosome number
-                    print("low chrom")
-                    continue # returns back to the top of the for loop
-                elif abs(reference0AlleleA/reference0_count-reference1AlleleA/reference1_count) < self.c:
-                    print('low allele freq')
-                    continue
-                else : # if passed all thresholds specified from config.ini
-                    chromosomeNumber= lineList[0][lineList[0].find('^[A-Z]'):]
-                    distanceToNextLocus = abs(currentLocus - int(lineList[1]))
-                    if (distanceToNextLocus > self.locusLength):
-                        print("low dist")
-                        for samplePopulation in range(self.refPanelNumber,len(populationColumnsList)): # find read count for each sample 
-                            for column in range(0,len(populationColumnsList[len(populationColumnsList)-1])):
-                                samPopCol = populationColumnsList[samplePopulation][column]
-                                # GT:AD:DP:GQ:PGT:PID:PL
-                                readCounts = lineList[samPopCol].split(':')[1].split(',')
-                                if len(readCounts) == 2:
+                        # compare to next population
+                        if (lineList[column2][:1] != '.'):
+                            if int(lineList[column2][:1])<=1:
+                                reference2AlleleA += int(lineList[column2][:1])
+                                reference2_count += 1
+                        
+                        if (lineList[column2][2:3] != '.'):
+                            if int(lineList[column2][2:3])<=1:
+                                reference2AlleleA += int(lineList[column2][2:3])
+                                reference2_count += 1
+                                    
+
+                    if reference1_count == 0:
+                        reference1_count = 1 # so theres no dividing by zero
+                    if reference2_count == 0:
+                        reference2_count = 1 # so theres no dividing by zero
+                    chrom_number1 = reference1_count
+                    chrom_number2 = reference2_count
+                    freq1=reference1AlleleA/reference1_count
+                    freq2=reference2AlleleA/reference2_count
+                    diff12=abs(freq1-freq2)
+                    if chrom_number1 < self.minChrom or chrom_number2 < self.minChrom: # make parameter the code for min chromosome number
+                        continue # returns back to the top of the for loop
+                    elif diff12 < self.c:
+                        continue
+                    else : 
+                        chromosomeNumber= lineList[0].split('[A-Z][a-z]')[0]
+                        if chromosomeNumber != pastChromosomeNumber:
+                            currentLocus = 0
+                        distanceToNextLocus = abs(currentLocus - int(lineList[1]))
+                        
+                        if (distanceToNextLocus > self.locusLength): # if passed all thresholds specified from config.ini
+                            # for printing
+                            alleleCountRefAltList.append(abs(reference1_count-reference1AlleleA))
+                            alleleCountRefAltList.append(reference1AlleleA)
+                            alleleCountRefAltList.append(abs(reference2_count-reference2AlleleA))
+                            alleleCountRefAltList.append(reference2AlleleA)
+                            for samplePopulation in range(self.refPanelNumber,len(populationColumnsList)): # find read count for each sample 
+                                for column in range(0,len(populationColumnsList[samplePopulation])):
+                                    try:
+                                        samPopCol = populationColumnsList[samplePopulation][column]
+                                    except IndexError as err:
+                                        print("Failed to load sample panel entry for (col:row)\t"+str(samplePopulation)+':'+str(column))
+                                        continue
+                                    # GT:AD:DP:GQ:PGT:PID:PL
+
+                                    readCounts = lineList[samPopCol].split(':')[1].split(',')
                                     for read in readCounts :
                                         readCountRefAltList.append(read)
-                
-                        currentLocus = int(lineList[1])
-                        recombinationFrequency=distanceToNextLocus*self.recombinationRate 
-                        # compute recombination frequency -- simple formula = (distance*recombinationRate)
-                        alleleCountRefAltListString ='\t'.join(str(e) for e in alleleCountRefAltList)
-                        readCountRefAltListString='\t'.join(str(e) for e in readCountRefAltList)
-                        print("{}\t{}\t{}\t{}\t{}\n".format(\
-                                    chromosomeNumber,currentLocus,alleleCountRefAltListString,recombinationFrequency,readCountRefAltListString))
+                    
+                            currentLocus = int(lineList[1])
+                            recombinationFrequency=distanceToNextLocus*self.recombinationRate 
+                            # compute recombination frequency -- simple formula = (distance*recombinationRate)
+                            alleleCountRefAltListString ='\t'.join(str(e) for e in alleleCountRefAltList)
+                            readCountRefAltListString='\t'.join(str(e) for e in readCountRefAltList)
+                            outFile.write("{}\t{}\t{}\t{}\t{}\n".format(\
+                                        chromosomeNumber,currentLocus,alleleCountRefAltListString,recombinationFrequency,readCountRefAltListString))
+                            self.lineCount +=1
+
 def main():
-    testObject = FreqDistanceCalculator()
+    usage="\nUsage: python createAncestryHMM-Input.py  \n"\
+                    +"Type python createAncestryHMM-Input.py -help to get descriptions of arguments\n "\
+                    +"You must have edited the config.ini file to read in parameters"
+    helpString = "[allelefreq cutoff]\n\t (Float) cutoff value for reference panel allele frequency calculation\n"\
+                +"\n[min locus distance]\n\t (Integer) minimum distance between each allele locus\n" \
+                +"\n[number of reference panels]\n\t (Integer) number of reference panel columns in the VCF.\n" \
+                +"\n[recombination_rate]\n\t (Float) estimated recombination rate for recombination probability for Ancestry_HMM input.\n\t Average recombinations per base pairs\n" \
+                +"\n[minChrom]\n\t (Integer) the minimum amount of chromosomes that must be present in the reference panel alleles to make it through the threshold\n" \
+                +"\n[filename]\n\t (String no quotes) Name of VCF file on local machine\n" \
+                +"\n[refPanel names(requires 2 names)]\n\t (at least 2 Strings no quotes) Names of reference panels in the VCF file.\n\t Reference panels should be named like guanaco0 guanaco1 guanaco2 etc.\n\t Example argument: guanaco\n" \
+                +"\n[sample names]\n\t (at least 2 Strings no quotes) Names of sample panels to be run in Ancestry_HMM. \n\t Should be named like llama1 llama2 llama3 etc.\n\t Example argument: llama\n"
+    numAboveC = 0
+    
+    
+    if (len(sys.argv) == 2):
+        if '-help' in sys.argv[1]:
+            print(helpString)
+            exit()
+    vcfInputProcessor = FreqDistanceCalculator()
     fileOut= input("Name of output file:")
-    testObject.createAncestryHMMInputFile(fileOut)    
-    print('Created output file:'+fileOut+" for Ancestry_HMM input")
+    vcfInputProcessor.createAncestryHMMInputFile(fileOut)    
+    print('Created output file: '+fileOut+" \n(for Ancestry_HMM input)\n"+str(vcfInputProcessor.lineCount)+" Lines")
 
 main()
